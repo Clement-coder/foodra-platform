d538c14ba3196225255f17fabe22f75fc9ca4325
Clement Raymond <chinexzy37@gmail.com>
2025-12-16 08:18:35 +0000

    added consistence styling for signup button

--- frontend/components/SignupButton.tsx
+++ frontend/components/SignupButton.tsx
@@ -9,7 +9,7 @@ export default function SignupButton() {
     <Button
       onClick={() => login()}
       disabled={!ready || authenticated}
-      className="px-4 py-2 bg-blue-600 text-white rounded"
+      className="px-4 py-2 bg-green-700 text-white rounded"
     >
       {authenticated ? "Already Signed In" : "Sign Up / Log In"}
     </Button>
